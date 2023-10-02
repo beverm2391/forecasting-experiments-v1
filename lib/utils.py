ef91c30415fef474ca1de8b1a2e8373d52e28a6f
beverm2391 <beverman4528@gmail.com>
2023-10-02 18:06:07 -0400

    added getsp500 func to lib with cacheing

--- lib/utils.py
+++ lib/utils.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 from pandas.tseries.holiday import USFederalHolidayCalendar
 from pandas.tseries.offsets import CustomBusinessHour
+from functools import lru_cache
 
 def log_returns(series):
     return np.log(series).diff()
@@ -15,3 +16,10 @@ def get_polygon_freq() -> pd.offsets.CustomBusinessDay:
 
 def get_hourly_market_freq() -> pd.offsets.CustomBusinessHour:
     return pd.offsets.CustomBusinessHour(calendar=USFederalHolidayCalendar(), start="9:30", end="16:00")
+
+@lru_cache # cache the result of this function to avoid refetching
+def get_sp500():
+    """Returns a dataframe of the S&P 500 companies."""
+    table = pd.read_html('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
+    df = table[0]
+    return df

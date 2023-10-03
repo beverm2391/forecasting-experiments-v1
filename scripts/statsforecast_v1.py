fc85af2fc8448ab3883f1a23fe7cf2b06c7b9dc4
beverm2391 <beverman4528@gmail.com>
2023-10-03 19:20:35 -0400

    regactored script be cause it was bad

--- scripts/statsforecast_v1.py
+++ scripts/statsforecast_v1.py
@@ -3,51 +3,19 @@ import numpy as np
 from statsforecast import StatsForecast
 from statsforecast.models import ARCH, GARCH, Naive
 import yfinance as yf
-from functools import lru_cache
 from typing import List, Union
 import random
+from time import perf_counter
 
 from lib.statsforecast import preprocess_yf
-from lib.utils import log_returns, try_it, time_it
+from lib.utils import log_returns
 
-data_path = "../data/tests/AAPL_MSFT_2018-01-01_2022-12-13_1d.pkl"
-df = pd.read_pickle(data_path)
-
-@try_it
-@time_it("Data fetched in")
-@lru_cache
 def fetch_data(tickers: Union[str, List[str]], start: str, end: str, interval: str = "1d") -> pd.DataFrame:
     """
     Fetches data from Yahoo Finance API
     """
-    return yf.download(tickers, start=start, end=end, interval=interval)
-
-@try_it
-@time_it("Data preprocessed in")
-def preprocess_data(*args, **kwargs): return preprocess_yf(*args, **kwargs)
-
-@try_it
-def model(df, models, freq, n_jobs=-1):
-    sf = StatsForecast(
-        df = df, 
-        models = models, 
-        freq = freq,
-        n_jobs = n_jobs
-    )
-    return sf
-
-@try_it
-@time_it("Forecasts generated in")
-def forecast(sf: StatsForecast, h: int, levels: List[int]):
-    levels = [80, 95] # confidence levels for the prediction intervals 
-    forecasts = sf.forecast(h=h, level=levels)
-    forecasts.reset_index(inplace=True)
-    return forecasts
-
-@try_it
-@time_it("Forecasts saved in")
-def save_forecasts(forecasts, path):
-    forecasts.to_pickle(path)
+    df = yf.download(tickers, start=start, end=end, interval=interval)
+    return df
 
 def main():
     # ! YOUR PARAMETERS HERE ================================================================
@@ -59,7 +27,11 @@ def main():
     interval = "1d"
 
     # models
-    models = [Naive, ARCH, GARCH]
+    models = [
+        Naive(),
+        ARCH(),
+        GARCH()
+    ]
 
     # forecasts
     horizon = 3
@@ -68,12 +40,41 @@ def main():
 
     # ! ====================================================================================
 
+    # Fetch data
+    print(f"Fetching data for {tickers} from {start} to {end} with interval {interval}...")
+    s = perf_counter()
 
     df = fetch_data(tickers, start, end, interval)
-    df = preprocess_data(df)
-    sf = model(df, models, freq="MS")
-    forecasts = forecast(sf, h=horizon, levels=levels if levels else None)
-    save_forecasts(forecasts, f"{path}_{random.randint(0, 999)}")
+
+    el = perf_counter() - s
+    print(f"Data fetched in {el:.02f}s")
+
+    # Preprocess data
+    print("Preprocessing data...")
+    df = preprocess_yf(df, log_returns)
+
+    # Forecast
+    print("Generating forecasts...")
+    s = perf_counter()
+
+    sf = StatsForecast(
+        df = df, 
+        models = models, 
+        freq = "D",
+        n_jobs = -1
+    )
+
+    forecasts = sf.forecast(h=horizon, level=levels)
+    forecasts.reset_index(inplace=True)
+
+    el = perf_counter() - s
+    print(f"Forecasts generated in {el:.02f}s")
+
+    # Save forecasts
+    print(f"Saving forecasts to {path}...")
+    forecasts.to_csv(f"{path}_forecast_{random.randint(0, 9999)}.csv")
+
+    print("Complete")
 
 if __name__ == "__main__":
     main()

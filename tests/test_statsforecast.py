bf3e8a5c69a67db16d5421dbe87a5a8b5158213e
beverm2391 <beverman4528@gmail.com>
2023-10-03 15:28:40 -0400

    updated tests to use saved data instead of cached

--- tests/test_statsforecast.py
+++ tests/test_statsforecast.py
@@ -1,24 +1,38 @@
 import pandas as pd
 import yfinance as yf
 from functools import lru_cache
+import os
 
 from lib.statsforecast import preprocess_yf
 from lib.utils import log_returns
 
+base_path = "/Users/beneverman/Documents/Coding/bp-quant/forecasting-experiments-v1/data/tests"
 
-@lru_cache
-def _cache_data_1():
-    df = yf.download(tickers='AAPL', start = '2018-01-01', end = '2022-12-31', interval='1d')
-    return df
+def get_data_1():
+    """
+    1 stock daily
+    """
+    fname = "AAPL_2018-01-01_2022-12-13_1d.csv"
+    fpath = f"{base_path}/{fname}"
+    if not os.path.exists(fpath):
+        df = yf.download('AAPL', start = '2018-01-01', end = '2022-12-31', interval='1d')
+        df.to_csv(fpath)
+    return pd.read_csv(fpath)
 
-@lru_cache
-def _cache_data_2():
-    df = yf.download(['AAPL', 'MSFT'], start = '2018-01-01', end = '2022-12-31', interval='1d')
-    return df
+
+def get_data_2():
+    """
+    2 stocks daily
+    """
+    fname = "AAPL_MSFT_2018-01-01_2022-12-13_1d.csv"
+    fpath = f"{base_path}/{fname}"
+    if not os.path.exists(fpath):
+        df = yf.download(['AAPL', 'MSFT'], start = '2018-01-01', end = '2022-12-31', interval='1d')
+        df.to_csv(fpath)
+    return pd.read_csv(fpath)
 
 def test_preprocess_yf():
-    data_1 = _cache_data_1()
+    data_1 = get_data_1()
     ppd_1 = preprocess_yf(data_1, log_returns)
-
-    data_2 = _cache_data_2()
+    data_2 = get_data_1()
     ppd_2 = preprocess_yf(data_2, log_returns)
